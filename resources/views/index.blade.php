6dab922b598551e27cfa911de6f21ac7a15b2244
rawanAbd960 <rawanimer12@gmail.com>
2021-12-20 12:35:45 +0200

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/css/uikit.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/><title>MITRS </title></head>
+<body class="uk-background-dark text-center lead text-white">
+    <div><div class="masthead-followup row m-0"><div class="col-12 col-md-12 p-3 p-md-5"><h1 class="animate__animated display-1 text-light animate__rubberBand"><strong></strong></h1>
+
+ <div class="uk-animation-toggle uk-animation-shake">
+     <a href="/login" class="uk-animation-toggle uk-animation-shake btn btn-light rounded-pill">Get Started →</a></div></div>
+    </body>
+    </html>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/login', 'Auth\LoginController@showLoginForm');
+
+Auth::routes();
+
+Route::get('users', 'UserController@index')->name('users.index');
+Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('products', 'ProductController')->middleware('auth');
+
+Route::resource('clients', 'ClientController')->middleware('auth');
+
+Route::resource('sales', 'SaleController')
+    ->except(['update', 'edit'])
+    ->middleware('auth');
+
+
+Route::get('/reports/sales', 'SaleController@export')->name('reports.sales')
+    ->middleware('auth');
