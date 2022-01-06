dacc9fdaea0fb040c84781cba0e87501c053b18a
shamssami <67718686+shamssami@users.noreply.github.com>
2022-01-06 21:12:28 +0200

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
     <div class="col-12 col-md-12 p-3 p-md-5 bg-dark"></div>
     <div class="col-12 col-md-12 p-3 p-md-5 text-warning bg-dark">
         <h1 class="display-1">MITRS</h1>
-    <h1  >Welcome to our POS system </h1>
- 
+    <h2  >Welcome to our POS system </h2>
+ 	
 
 </div>
 
